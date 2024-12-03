1187ee1858cbbf13bf0bb349a1e2cdd9fa08ca6f
tpoiesz_altennl <tristan.poiesz@alten.nl>
2024-12-03 14:08:22 +0100

    Change day2 solution and file util
    - Day 2 solution now skips bad levels instead of necessarily retrying the whole sequence with the bad level removed (it still retries if the skip does not fix enough, for edge cases that the skip does not catch)
    - File util can now output to a debug file, to see which test cases match or do not pass the test

--- AoCSolutions/Day2/Day2.cpp
+++ AoCSolutions/Day2/Day2.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include "../Util.h"
 
-bool isSafe(const std::vector<int>& levels, bool maySkip = false)
+bool IsSafe(const std::vector<int>& levels, bool maySkip = false)
 {
 	if (levels.size() < 2)
 	{
@@ -23,16 +23,32 @@ bool isSafe(const std::vector<int>& levels, bool maySkip = false)
 			{
 				if (skipUsed || !maySkip)
 				{
-					return false;
+					if (!maySkip)
+						return false;
+					else
+					{
+						std::vector<int> fixedLevels = levels;
+						// Try skipping the first conflicting level instead of the second one
+						fixedLevels.erase(fixedLevels.begin() + prevIndex);
+						if (IsSafe(fixedLevels, false))
+							return true;
+						else
+						{
+							// Try skipping the first level
+							fixedLevels = levels;
+							fixedLevels.erase(fixedLevels.begin());
+							return IsSafe(fixedLevels, false);
+						}
+					}
 				}
 				else
 				{
 					skipUsed = true;
+					// Let prevIndex trail when using a skip
 				}
 			}
 			else
 			{
-				// Let prevIndex trail when using a skip
 				prevIndex = i;
 			}
 		}
@@ -42,16 +58,32 @@ bool isSafe(const std::vector<int>& levels, bool maySkip = false)
 			{
 				if (skipUsed || !maySkip)
 				{
-					return false;
+					if (!maySkip)
+						return false;
+					else
+					{
+						std::vector<int> fixedLevels = levels;
+						// Try skipping the first conflicting level instead of the second one
+						fixedLevels.erase(fixedLevels.begin() + prevIndex);
+						if (IsSafe(fixedLevels, false))
+							return true;
+						else
+						{
+							// Try skipping the first level
+							fixedLevels = levels;
+							fixedLevels.erase(fixedLevels.begin());
+							return IsSafe(fixedLevels, false);
+						}
+					}
 				}
 				else
 				{
 					skipUsed = true;
+					// Let prevIndex trail when using a skip
 				}
 			}
 			else
 			{
-				// Let prevIndex trail when using a skip
 				prevIndex = i;
 			}
 		}
@@ -86,7 +118,7 @@ int Day2::Solution1()
 	unsigned int numSafe = 0;
 	for (std::vector<int> levels : reports)
 	{
-		if (isSafe(levels))
+		if (IsSafe(levels))
 			++numSafe;
 	}
 
@@ -105,52 +137,26 @@ int Day2::Solution2()
 		return 1;
 	}
 
-	std::vector<std::vector<int>> reports;
+	std::vector<horizontal_vector<int>> reports;
 	std::string reportString;
 	while (std::getline(inFile, reportString))
 	{
 		std::stringstream ss(reportString);
-		std::vector<int> levels{
-			std::istream_iterator<int>(ss),
-			std::istream_iterator<int>()
-		};
+		int temp = 0;
+		horizontal_vector<int> levels;
+		while (ss >> temp)
+			levels.push_back(temp);
 		reports.push_back(levels);
 	}
 
-	//FileUtil::OutputMatches<std::vector<int>, bool>(__FILE__, reports, &isSafe, true, true);
+	FileUtil::DebugFile dbg(__FILE__);
+	dbg.OutputMatches<horizontal_vector<int>, bool>(reports, &IsSafe, false, true);
 
-	int count = 0;
 	unsigned int numSafe = 0;
 	for (std::vector<int> levels : reports)
 	{
-		++count;
-		if (isSafe(levels, true))
+		if (IsSafe(levels, true))
 			++numSafe;
-		else
-		{
-			// If only the first level is wrong, we need to check that separately
-			std::vector<int> fixedLevels = levels;
-			fixedLevels.erase(fixedLevels.begin());
-			if (isSafe(fixedLevels, false))
-				++numSafe;
-			else
-			{
-				// Could be that our assumption for ascending/descending got screwed up by the second level being wrong, 
-				// so we might need to check again with the second level removed
-				fixedLevels = levels;
-				if ((fixedLevels[1] >= fixedLevels[0] && fixedLevels[2] < fixedLevels[0]) ||
-					(fixedLevels[1] <= fixedLevels[0] && fixedLevels[2] > fixedLevels[0]))
-				{
-					fixedLevels.erase(fixedLevels.begin() + 1);
-					if (isSafe(fixedLevels, false))
-						++numSafe;
-					else
-					{
-						std::cout << count << '\n';
-					}
-				}
-			}
-		}
 	}
 
 	std::cout << numSafe;
--- AoCSolutions/Util.h
+++ AoCSolutions/Util.h
@@ -30,26 +30,38 @@ public:
 		return file;
 	}
 
-	template<typename T, typename... Args>
-	static void OutputMatches(std::string callingLocation, const std::vector<T>& testCases, std::function<bool(const T&, Args...)> conditional, 
-		bool matchConditional = true, Args... argsForConditional)
+	class DebugFile
 	{
-		std::ofstream file(MakeFolder(callingLocation) + "/OutputFile.txt");
-		if (!file)
+	public:
+		DebugFile(const std::string callingLocation)
 		{
-			std::cout << "Oh no!\n";
+			m_File = std::ofstream(FileUtil::MakeFolder(callingLocation) + "/OutputFile.txt");
+		}
+		~DebugFile()
+		{
+			m_File.close();
 		}
 
-		for (const T& testCase : testCases)
+		template<typename T, typename... Args>
+		void OutputMatches(const std::vector<T>& testCases, std::function<bool(const T&, Args...)> conditional,
+			bool matchConditional = true, Args... argsForConditional)
 		{
-			if (conditional(testCase, argsForConditional...) == matchConditional)
+			if (!m_File)
 			{
-				std::stringstream ss;
-				//ss << testCase;
-				//file.write(ss.str());
+				std::cout << "Oh no!\n";
+			}
+
+			for (const T& testCase : testCases)
+			{
+				if (conditional(testCase, argsForConditional...) == matchConditional)
+				{
+					m_File << testCase << std::endl;
+				}
 			}
 		}
-	}
+	private:
+		std::ofstream m_File;
+	};
 
 private:
 	static std::string MakeFolder(const std::string& filepath)
@@ -59,15 +71,30 @@ private:
 		secLastSep = filepath.rfind('\\', lastSep - 1) + 1;
 		return filepath.substr(secLastSep, lastSep - secLastSep);
 	}
-
-	template<typename T>
-	static std::stringstream MakeSSFromVector(const std::vector<T>& vec)
-	{
-		std::stringstream ret;
-		for (const T& elem : vec)
-		{
-			ret << elem << ' ';
-		}
-		return ret;
-	}
 };
+
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, std::vector<T> operand)
+{
+	for (const T& elem : operand)
+	{
+		stream << elem << '\n';
+	}
+	return stream;
+}
+
+template<class T>
+class horizontal_vector : public std::vector<T>
+{
+	// This class needs no body; it only exists to differentiate vectors that need to be printed horizontally
+};
+
+template<typename T>
+std::ostream& operator<<(std::ostream& stream, horizontal_vector<T> operand)
+{
+	for (const T& elem : operand)
+	{
+		stream << elem << ' ';
+	}
+	return stream;
+}
