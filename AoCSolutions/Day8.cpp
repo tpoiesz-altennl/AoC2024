baea8437a98454efe9b11c25c85184ee459ec191
tpoiesz_altennl <tristan.poiesz@alten.nl>
2024-12-09 15:48:35 +0100

    Add start to day 8
    Add initial files

---
+++ AoCSolutions/Day8.cpp
@@ -0,0 +1,11 @@
+#include "Day8.h"
+
+int Day8::Solution1()
+{
+
+}
+
+int Day8::Solution2()
+{
+
+}
---
+++ AoCSolutions/Day8.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class Day8
+{
+public:
+	static int Solution1();
+	static int Solution2();
+};
